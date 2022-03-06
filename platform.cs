037343636d73f9210fbf3d2e8b69f5639b7688b2
Byun Jaehee <bbj22502@naver.com>
2022-03-06 23:52:11 +0900

    background
    background

--- platform.cs
+++ platform.cs
@@ -1,37 +1,30 @@
 using UnityEngine;
 
-// 발판으로서 필요한 동작을 담은 스크립트
-public class Platform : MonoBehaviour {
-    public GameObject[] obstacles; // 장애물 오브젝트들
-    private bool stepped = false; // 플레이어 캐릭터가 밟았었는가
+// 왼쪽 끝으로 이동한 배경을 오른쪽 끝으로 재배치하는 스크립트
+public class BackgroundLoop : MonoBehaviour {
+    private float width; // 배경의 가로 길이
 
-    // 컴포넌트가 활성화될때 마다 매번 실행되는 메서드
-    private void OnEnable() 
-    {
-        stepped=false;
+    private void Awake() {
 
-        for(int i=0;i<obstacles.Length;i++)
-        {
-            if(Random.Range(0,3)==0)
-            {
-                obstacles[i].SetActive(true);
-            }
-            else
-            {
-                obstacles[i].SetActive(false);
-
-            }
-        }
-        // 발판을 리셋하는 처리
+        BoxCollider2D backgroundCollider = GetComponent<BoxCollider2D>();
+        width= backgroundCollider.size.x;
+        // 가로 길이를 측정하는 처리
     }
 
-    void OnCollisionEnter2D(Collision2D collision) 
-    {
-        if(collision.collider.tag=="Player"&&!stepped)
+    private void Update() {
+
+        if(transform.position.x <= -width)
         {
-            stepped=true;
-            GameManager.instance.AddScore(1);
+            Reposition();
         }
-        // 플레이어 캐릭터가 자신을 밟았을때 점수를 추가하는 처리
+        // 현재 위치가 원점에서 왼쪽으로 width 이상 이동했을때 위치를 리셋
+    }
+
+    // 위치를 리셋하는 메서드
+    private void Reposition() {
+
+        Vector2 offset= new Vector2(width*2f,0);
+        transform.position=(Vector2)transform.position+offset;
+        
     }
 }
