449ccb6ec85664a2f8a1976a7d8c66490f16dd40
Byun Jaehee <bbj22502@naver.com>
2022-03-16 23:42:20 +0900

    Update platform.cs
    platform

--- platform.cs
+++ platform.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 
 // 왼쪽 끝으로 이동한 배경을 오른쪽 끝으로 재배치하는 스크립트
-public class BackgroundLoop : MonoBehaviour {
+public class BackgroundLoop : MonoBehaviour 
+
+{
     private float width; // 배경의 가로 길이
 
     private void Awake() {
